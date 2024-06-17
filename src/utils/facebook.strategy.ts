b0a4e2bce659e0e7d568757c64d9d0cb7b31b155
Sajith Madushanka <99092639+sajithmadushanka@users.noreply.github.com>
2024-06-17 10:36:50 +0530

    Update facebook.strategy.ts

--- src/utils/facebook.strategy.ts
+++ src/utils/facebook.strategy.ts
@@ -11,9 +11,9 @@ export class FacebookStrategy extends PassportStrategy(Strategy, 'facebook') {
     private readonly configService: ConfigService,
   ) {
     super({
-      clientID: '828936549151783',
-      clientSecret: 'bb445a3149750b0d2cc07677d043a53d',
-      callbackURL: 'https://3000-sajithmadushank-oauth20-r84wtfxdn7i.ws-us114.gitpod.io/api/v1/auth/facebook/redirect',
+      clientID: '',
+      clientSecret: '',
+      callbackURL: '',
       profileFields: ['id', 'name', 'emails', 'photos'],
     });
   }
